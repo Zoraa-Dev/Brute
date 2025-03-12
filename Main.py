c8103805e5a3f24857a6d5ba17c1708c39f649aa
Zoraa Dev <132505748+Zoraa-Dev@users.noreply.github.com>
2025-03-13 02:26:33 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,2 +1,12 @@
 from Penyimpanan.Database.Instagram import Main
-Main().Pengecekan_Data()
+
+class Freestyle:
+  def __init__(self) -> None:
+    pass
+
+  def ZoraaDev(self):
+    try: Main().Pengecekan_Data()
+    except (Exception) as e: exit(e)
+
+
+Freestyle().ZoraaDev()
